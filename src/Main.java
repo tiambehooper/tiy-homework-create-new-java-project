22cc91aa8f433dd8dced15c3c67f4c6fe9caa85a
Tiambe Hooper <tiambe48@gmail.com>
2017-03-04 14:52:45 -0500

    Uesed different expresstion and Printed Out Resluts and documented program well

---
+++ src/Main.java
@@ -0,0 +1,137 @@
+/*
+        Program:	~/Projects/tiy-home-create-new-java-project
+        Programmer:	Tiambe Hooper
+        Course:		Java Back-End Engineering   https://www.theironyard.com/courses#back-end-engineering
+        Chapter:	Day 3 (03-03-17)
+        Excrise:	Main
+        Date Due:	03-04-17
+        Purpose:	This class call Main will print different expressions. And I hope it is well documented too!
+
+        */
+
+/* The name of my class is Main it dose not have to start with an uppercase but you
+   should start it for good
+   Programming Paractce */
+
+
+public class Main{
+
+
+    //  This is the entry point and every project must have at least one main Method
+    public static void main(String[] args) {
+
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 -Double, Double
+         Returns :Results
+         Purpose is using the Addtion Operator(+)
+         *********************************************************************/
+
+        System.out.println(-.44545451 +  6565656.6000000);
+
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 Double, Double
+         Returns :Results
+         Purpose is using the Subtration Operator(-)
+         *********************************************************************/
+
+
+        System.out.println(2.99999 - 2.888888);
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 Integer, Integer, Integer
+         Returns: Results
+         Purpose is using the Addition, Subtraction, Division Operator
+         *********************************************************************/
+
+
+        System.out.println(12 / + 9 - 2);
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 Boolean, Boolean
+         Returns :Results
+         Purpose is using the Equality Operator (==)
+         *********************************************************************/
+
+        System.out.println(true == false);
+
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 String
+         Returns : Results
+         Purpose: Is to  output a String(" ....")
+         *********************************************************************/
+
+
+        System.out.println("Hi my name is Tiambe Hooper: My age is =\t  12");
+
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 Boolean, Boolean
+         Returns: Results
+         Purpose: Use  the Logical Operators (!=)
+         *********************************************************************/
+
+
+        System.out.println(false != true);
+
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 Integer, Integer, Double, Integer
+         Returns :Results
+         Purpose: Operations Performed ( %,+,*)
+         *********************************************************************/
+
+
+        System.out.println(999999 % 534343 + 555.444 * 2);
+
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 -Interger,  -Double
+         Returns :Results
+         Purpose: Subtraction  (-)
+         *********************************************************************/
+
+
+
+        System.out.println(- 3433545 - 45545.4444);
+
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 Double, -Integer
+         Returns :Results
+         Purpose: Use  the Logical Operators (*)
+         *********************************************************************/
+
+
+        System.out.println(545454545.999999 *  -2.00000);
+
+        /*********************************************************************
+         Method: Stystem.out.println()
+         Given:	 String
+         Returns :Results
+         Purpose: Output a String
+         *********************************************************************/
+
+
+        System.out.println("\n A REPL is an interactive environment that allows you to run individual lines of code and immediately see results. REPL is an acronym standing for:\n" +
+                "\n" +
+                "Read - Read a line of code input by the user\n" +
+                "Evaluate - Run the code immediately\n" +
+                "Print - Print out the results of running the code\n" +
+                "Loop - Wait for the user to input more code");
+
+
+    }
+
+}
